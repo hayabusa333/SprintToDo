60fb9ce6e86930bc9bec89d41e40f4b55d4f40c4
hayabusa333 <junnichi0121@gmail.com>
2014-07-28 22:22:19 +0900

    sprint sort start time

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -4,7 +4,7 @@ class SprintsController < ApplicationController
   # GET /sprints
   # GET /sprints.json
   def index
-    @sprints = Sprint.all
+    @sprints = Sprint.order("start")
   end
 
   # GET /sprints/1
