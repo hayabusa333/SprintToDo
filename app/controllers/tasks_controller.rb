29861328391f2932c1804ef1e8f19f4d89dd34e8
hayabusa333 <junnichi0121@gmail.com>
2014-10-28 23:22:55 +0900

    tasks times input form add

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -79,6 +79,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:name, :status, :memo)
+      params.require(:task).permit(:name, :status, :hour, :minute, :memo)
     end
 end
